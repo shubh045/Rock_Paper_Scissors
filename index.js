c35b9af3a5ddbcffcb92ccdef5f457acdaa905ae
shubh045 <shubh6441@gmail.com>
2021-09-30 16:57:27 +0530

    defined event listener for buttons

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
-function computerPlay(){
+const buttons = document.querySelectorAll('button');
+const div = document.querySelector('div');
+const ul = document.createElement('ul');
+const li = document.createElement('li');
+
+// ul.appendChild(li);
+// div.appendChild(li);
+
+function computerPlay() {
     let arr = ['Rock', 'Paper', 'Scissors'];
-    let computerChoice = arr[Math.floor(Math.random()*arr.length)];
+    let computerChoice = arr[Math.floor(Math.random() * arr.length)];
     return computerChoice;
 }
 
-function playRound(playerSelection, computerSelection){
+function playRound(playerSelection, computerSelection) {
     console.log(playerSelection);
     console.log(computerSelection);
     let Paper = 'paper'.toUpperCase();
@@ -12,27 +20,39 @@ function playRound(playerSelection, computerSelection){
     let Scissors = 'scissors'.toUpperCase();
     let ps = playerSelection.toUpperCase();
     let cs = computerSelection.toUpperCase();
-    if(ps === Rock && cs === Paper)
-    return 'You lose! Paper beats Rock';
-    else if(ps === Rock && cs === Scissors)
-    return 'You win! Rock beats Scissors';
-    if(ps === Scissors && cs === Paper)
-    return 'You win! Scissors beat paper';
-    else if(ps === Scissors && cs === Rock)
-    return 'you lose';
-    else if(ps === Paper && cs === Rock)
-    return 'you win';
-    else if(ps === Paper && cs === Scissors)
-    return 'you lose';
-    else if(ps === ' ')
-    return 'Please enter correct value';
-    else 
-    return 'tie';
+    if (ps === Rock && cs === Paper)
+        return 'You lose! Paper beats Rock';
+    else if (ps === Rock && cs === Scissors)
+        return 'You win! Rock beats Scissors';
+    if (ps === Scissors && cs === Paper)
+        return 'You win! Scissors beat paper';
+    else if (ps === Scissors && cs === Rock)
+        return 'you lose';
+    else if (ps === Paper && cs === Rock)
+        return 'you win';
+    else if (ps === Paper && cs === Scissors)
+        return 'you lose';
+    else if (ps === ' ')
+        return 'Please enter correct value';
+    else
+        return 'tie';
 }
 
-function game(){
-        let playerSelection = prompt("Enter your selection");
-        return (playRound(playerSelection, computerPlay()));  
-}
-for(let i=0; i<5; i++)
-console.log(game());
+// function game() {
+//     let playerSelection = prompt("Enter your selection");
+//     return (playRound(playerSelection, computerPlay()));
+// }
+
+const userSelection = event => {
+    const newDiv = document.createElement('div');
+    const li = document.createElement('li');
+    const userInput = document.createTextNode(event);
+    newDiv.appendChild(li);
+    li.appendChild(userInput);
+    ul.appendChild(li);
+    div.appendChild(ul);
+};
+
+buttons.forEach(button => {
+    button.addEventListener('click', (e) => userSelection(e.target.value))
+});
